812530c00c251ccecebf1874c5f28f7496afcf60
Malinder <malinder@codilar.com>
2017-08-08 11:09:24 +0530

    unit test folder

---
+++ Test/Unit/Block/Navigation/FilterRendererTest.php
@@ -0,0 +1,70 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Blackhat
+ * Date: 08/08/2017
+ * Time: 00:59
+ */
+
+namespace Codilar\LayeredNavigation\Test\Unit\Block\Navigation;
+use Codilar\LayeredNavigation\Block\Navigation\FilterRenderer;
+
+class FilterRendererTest extends \PHPUnit_Framework_TestCase {
+
+    /**
+     * @var FilterRenderer
+     */
+
+    protected $filter;
+
+    protected $registry;
+    protected $_helper,$_categoryFactory,$_productCollectionFactory,$_category;
+    protected $filterRendererClass;
+
+    public function setUp() {
+
+        $this->registry = $this->getMockBuilder(\Magento\Framework\Registry::class)
+            ->disableOriginalConstructor()->getMock();
+
+        $this->_helper = $this->getMockBuilder(\Codilar\LayeredNavigation\Helper\Data::class)
+            ->disableOriginalConstructor()->getMock();
+
+        $this->_categoryFactory = $this->getMockBuilder(\Magento\Catalog\Model\CategoryFactory::class)
+            ->setMethods(['create'])
+            ->disableOriginalConstructor()->getMock();
+
+        $this->_productCollectionFactory = $this->getMockBuilder(\Magento\Catalog\Model\ResourceModel\Product\CollectionFactory::class)
+            ->setMethods(['create'])
+            ->disableOriginalConstructor()->getMock();
+
+        $this->_category = $this->getMockBuilder(\Magento\Catalog\Model\Category::class)
+            ->disableOriginalConstructor()->getMock();
+
+        $objectManager = new \Magento\Framework\TestFramework\Unit\Helper\ObjectManager($this);
+        $this->filterRendererClass = $objectManager->getObject(
+            \Codilar\LayeredNavigation\Block\Navigation\FilterRenderer::class,
+            [
+                'registry' => $this->registry,
+                'helper' => $this->_helper,
+                'categoryFactory' => $this->_categoryFactory,
+                'productCollectionFactory' => $this->_productCollectionFactory
+            ]
+        );
+
+    }
+
+    public function testGetCategory() {
+
+        $this->_category->expects($this->any())
+            ->method('load')
+            ->with(4)
+            ->will($this->returnValue($this->_category));
+
+        $this->_categoryFactory->expects($this->any())
+            ->method('create')
+            ->will($this->returnValue($this->_category));
+
+        $this->assertEquals('object', gettype($this->filterRendererClass->getCategory(4)));
+    }
+
+}
---
+++ Test/Unit/Helper/DataTest.php
@@ -0,0 +1,106 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Blackhat
+ * Date: 04/08/2017
+ * Time: 19:11
+ */
+
+namespace Codilar\LayeredNavigation\Test\Unit\Helper;
+use Codilar\LayeredNavigation\Helper\Data;
+
+class DataTest extends \PHPUnit_Framework_TestCase {
+
+    /**
+     * @var Data
+     */
+
+    protected $url;
+
+    /**
+     * @var string
+     */
+    protected $expectedMessage;
+    protected $categoryFactory;
+    protected $registry;
+    protected $productAttributeRepository;
+    protected $_url,$request,$scopeConfig;
+    protected $dataClass,$category;
+
+    public function setUp() {
+
+        $this->categoryFactory = $this->getMockBuilder(\Magento\Catalog\Model\CategoryFactory::class)
+            ->setMethods(['create'])
+            ->disableOriginalConstructor()->getMock();
+
+        $this->registry = $this->getMockBuilder(\Magento\Framework\Registry::class)
+            ->disableOriginalConstructor()->getMock();
+
+        $this->productAttributeRepository = $this->getMockBuilder(\Magento\Catalog\Model\Product\Attribute\Repository::class)
+            ->disableOriginalConstructor()->getMock();
+
+        $this->_url = $this->getMockBuilder(\Magento\Framework\UrlInterface::class)
+            ->disableOriginalConstructor()->getMock();
+
+        $this->request = $this->getMockBuilder(\Magento\Framework\App\Request\Http::class)
+            ->disableOriginalConstructor()->getMock();
+
+        $this->scopeConfig = $this->getMockBuilder(\Magento\Framework\App\Config\ScopeConfigInterface::class)
+            ->disableOriginalConstructor()->getMock();
+
+        $this->category = $this->getMockBuilder('Magento\Catalog\Model\Category')
+            ->disableOriginalConstructor()
+            ->setMethods(['load'])
+            ->getMock();
+
+        $objectManager = new \Magento\Framework\TestFramework\Unit\Helper\ObjectManager($this);
+        $this->dataClass = $objectManager->getObject(
+            \Codilar\LayeredNavigation\Helper\Data::class,
+            [
+                'registry' => $this->registry,
+                'productAttributeRepository' => $this->productAttributeRepository,
+                'url' => $this->_url,
+                'request' => $this->request,
+                'scopeConfig' => $this->scopeConfig,
+                'categoryFactory' => $this->categoryFactory
+            ]
+        );
+
+    }
+
+    public function testGetConfig() {
+
+        $this->scopeConfig
+            ->method('getValue')
+            ->willReturn(1);
+
+        $this->assertEquals(1, $this->dataClass->getConfig('codilar_layered/general/enable'));
+    }
+
+    public function testGetUrl() {
+
+        $this->category->expects($this->any())
+            ->method('load')
+            ->with(4)
+            ->will($this->returnValue($this->category));
+
+        $this->category->expects($this->any())
+            ->method('getName')
+            ->will($this->returnValue('beads'));
+
+        $this->category->expects($this->once())
+            ->method('getPath')
+            ->will($this->returnValue($this->category));
+
+        $this->categoryFactory->expects($this->once())
+            ->method('create')
+            ->will($this->returnValue($this->category));
+
+        $this->_url->expects($this->once())
+            ->method('getUrl')
+            ->will($this->returnValue('http://localhost/extension'));
+
+        $this->assertEquals('http://localhost/extension/charms/beads.html', $this->dataClass->getUrl(4));
+    }
+
+}
